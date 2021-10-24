03cdd70245b8972d0bf898f08a49b06d70cc786b
aserravalle <ariel.serravalle@gmail.com>
2021-10-24 11:29:28 +1100

    Refactor: Implement new getId method in IEntity

--- src/test/java/dungeonmania/actions/BattleTest.java
+++ src/test/java/dungeonmania/actions/BattleTest.java
@@ -1,6 +1,17 @@
 package dungeonmania.actions;
 
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.entities.movingEntities.CharacterEntity;
+import dungeonmania.entities.movingEntities.ZombieToastEntity;
+
 public class BattleTest {
-    
+    @Test
+    public void TestBattle() {
+        CharacterEntity character = new CharacterEntity();
+        ZombieToastEntity zombie = new ZombieToastEntity();
+
+        new Battle(character, zombie);
+    }
     
 }

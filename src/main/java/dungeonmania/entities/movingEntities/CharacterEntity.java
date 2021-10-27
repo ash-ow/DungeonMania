2ac7a6578816a5fe214d4bb7c4d5c190f48ebe2e
RadarChallenger <radara350@gmail.com>
2021-10-27 15:53:52 +1100

    functionality: fixed character movement when spider on wall

--- src/main/java/dungeonmania/entities/movingEntities/CharacterEntity.java
+++ src/main/java/dungeonmania/entities/movingEntities/CharacterEntity.java
@@ -89,14 +89,14 @@ public class CharacterEntity extends Entity implements IMovingEntity, IBattlingE
         Position target = position.translateBy(direction);
         List<IEntity> targetEntities = entitiesControl.entitiesFromPosition(target);
         List<IInteractingEntity> targetInteractable = entitiesControl.entitiesInteractableInRange(targetEntities);
-        // if (!EntitiesControl.interactingEntitiesUnpassable(targetInteractable)) {
-        //     return;
-        // }
         boolean interacted = false;
         for (IInteractingEntity entity : targetInteractable) { // Slight bug if player interacts with many things stacked on top of each other- keeps moving
             if (entity.interactWithPlayer(entitiesControl, direction, this)) {
                 interacted = true;
-                this.move(direction);
+                List<IEntity> newTargetEntities = entitiesControl.entitiesFromPosition(target);
+                if (!EntitiesControl.entitiesUnpassable(newTargetEntities)) {
+                    this.move(direction);
+                }
             }
         }
         if ((targetEntities.size() == 0) || (!EntitiesControl.entitiesUnpassable(targetEntities) && !interacted)) {

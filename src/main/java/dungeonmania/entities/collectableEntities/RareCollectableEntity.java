966303d2a18cb1ff9e939b72be11563ccf0de3cc
RadarChallenger <radara350@gmail.com>
2021-10-28 00:39:17 +1100

    test: added info response test for rarecollectable entities (not implemented)

--- src/main/java/dungeonmania/entities/collectableEntities/RareCollectableEntity.java
+++ src/main/java/dungeonmania/entities/collectableEntities/RareCollectableEntity.java
@@ -1,7 +1,22 @@
 package dungeonmania.entities.collectableEntities;
 
-public abstract class RareCollectableEntity extends CollectableEntity {
-    protected RareCollectableEntity(int x, int y, int layer, String type) {
-        super(x, y, layer, type);
+import dungeonmania.entities.movingEntities.CharacterEntity;
+
+public class RareCollectableEntity extends CollectableEntity {
+    // TODO this class is not implemented, current state is used to pass test
+    public RareCollectableEntity(int x, int y, int layer) {
+        super(x, y, layer, "one_ring");
+    }
+
+    @Override
+    public void used(CharacterEntity player) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isPassable() {
+        // TODO Auto-generated method stub
+        return false;
     }    
 }
--- src/test/java/dungeonmania/entities/collectableEntityTests/RareCollectableEntityTests.java
+++ src/test/java/dungeonmania/entities/collectableEntityTests/RareCollectableEntityTests.java
@@ -2,11 +2,22 @@ package dungeonmania.entities.collectableEntityTests;
 
 import org.junit.jupiter.api.Test;
 
-public class RareCollectableEntityTests extends CollectableEntityTests {
-    // TODO add test for this class
-    @Test
-    public void test() {
+import dungeonmania.entities.collectableEntities.RareCollectableEntity;
+import dungeonmania.util.Position;
 
+public class RareCollectableEntityTests extends CollectableEntityTests {
+
+    @Override
+    @Test
+    public void TestEntityResponseInfo() {
+        RareCollectableEntity key = new RareCollectableEntity(0,0,0);
+        assertEntityResponseInfoEquals(
+            key,
+            "one_ring-0-0-0",
+            "one_ring",
+            new Position(0,0),
+            false
+        );
     }
     
 }

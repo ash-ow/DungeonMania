342486b819d1db78493d9632029334e4d90be2bf
Wilson Lin <wilson.lin219@gmail.com>
2021-10-27 14:01:43 +1100

    Completed: Collectable Entities pickup tests (moved to appropriate file)

--- src/test/java/dungeonmania/entities/collectableEntityTests/CollectableEntityTests.java
+++ src/test/java/dungeonmania/entities/collectableEntityTests/CollectableEntityTests.java
@@ -1,28 +1,16 @@
 package dungeonmania.entities.collectableEntityTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
-import dungeonmania.dungeon.Dungeon;
-import dungeonmania.dungeon.EntitiesControl;
-import dungeonmania.entities.IEntity;
 import dungeonmania.util.Position;
 import dungeonmania.entities.collectableEntities.ArrowsEntity;
 import dungeonmania.entities.collectableEntities.SwordEntity;
 import dungeonmania.entities.collectableEntities.WoodEntity;
 import dungeonmania.entities.collectableEntities.BombEntity;
 import dungeonmania.entities.collectableEntities.ArmourEntity;
-import dungeonmania.util.Direction;
-import dungeonmania.response.models.ItemResponse;
-import java.util.List;
-import dungeonmania.entities.IEntity;
 import dungeonmania.entities.IEntityTests;
-import dungeonmania.entities.movingEntities.CharacterEntity;
 
 public class CollectableEntityTests implements IEntityTests {
     @Override
@@ -43,87 +31,6 @@ public class CollectableEntityTests implements IEntityTests {
         ArmourEntity armour = new ArmourEntity(0,4,0);
         assertEntityResponseInfoEquals(armour, "armour-0-4-0", "armour", new Position(0,4), false);
     }
-    
-    @Test
-    public void TestWoodCollect() {
-        CharacterEntity player = new CharacterEntity(0, 0, 0);
-        WoodEntity wood = new WoodEntity(0,1,0);
-        EntitiesControl entities = new EntitiesControl();
-        assertEntityResponseInfoEquals(wood, "wood-0-1-0", "wood", new Position(0,1), false);
-        
-        entities.addEntities(wood);
-        assertTrue(entities.getEntities().size() == 1);
-
-        wood.interactWithPlayer(entities, Direction.RIGHT, player);
-
-        assertTrue(player.getInventoryInfo().size() > 0);
-        assertTrue(entities.getEntities().size() == 0);
-    }
-
-    @Test
-    public void TestArrowsCollect() {
-        CharacterEntity player = new CharacterEntity(0, 0, 0);
-        ArrowsEntity arrows = new ArrowsEntity(0,0,0);
-        EntitiesControl entities = new EntitiesControl();
-        assertEntityResponseInfoEquals(arrows, "arrows-0-0-0", "arrows", new Position(0,0), false);
-        
-        entities.addEntities(arrows);
-        assertTrue(entities.getEntities().size() == 1);
-
-        arrows.interactWithPlayer(entities, Direction.RIGHT, player);
-
-        assertTrue(player.getInventoryInfo().size() > 0);
-        assertTrue(entities.getEntities().size() == 0);
-    }
-
-    @Test
-    public void TestBombCollect() {
-        CharacterEntity player = new CharacterEntity(0, 0, 0);
-        BombEntity bomb = new BombEntity(0,0,0);
-        EntitiesControl entities = new EntitiesControl();
-        assertEntityResponseInfoEquals(bomb, "bomb-0-0-0", "bomb", new Position(0,0), false);
-        
-        entities.addEntities(bomb);
-        assertTrue(entities.getEntities().size() == 1);
-
-        bomb.interactWithPlayer(entities, Direction.RIGHT, player);
-
-        assertTrue(player.getInventoryInfo().size() > 0);
-        assertTrue(entities.getEntities().size() == 0);
-    }
-
-    @Test
-    public void TestSwordCollect() {
-        CharacterEntity player = new CharacterEntity(0, 0, 0);
-        SwordEntity sword = new SwordEntity(0,0,0);
-        EntitiesControl entities = new EntitiesControl();
-        assertEntityResponseInfoEquals(sword, "sword-0-0-0", "sword", new Position(0,0), false);
-        
-        entities.addEntities(sword);
-        assertTrue(entities.getEntities().size() == 1);
-
-        sword.interactWithPlayer(entities, Direction.RIGHT, player);
-
-        assertTrue(player.getInventoryInfo().size() > 0);
-        assertTrue(entities.getEntities().size() == 0);
-    }
-
-    @Test
-    public void TestArmourCollect() {
-        CharacterEntity player = new CharacterEntity(0, 0, 0);
-        ArmourEntity armour = new ArmourEntity(0,0,0);
-        EntitiesControl entities = new EntitiesControl();
-        assertEntityResponseInfoEquals(armour, "armour-0-0-0", "armour", new Position(0,0), false);
-        
-        entities.addEntities(armour);
-        assertTrue(entities.getEntities().size() == 1);
-
-        armour.interactWithPlayer(entities, Direction.RIGHT, player);
-
-        assertTrue(player.getInventoryInfo().size() > 0);
-        assertTrue(entities.getEntities().size() == 0);
-    }
-
 }
 
 
--- src/test/java/dungeonmania/entities/collectableEntityTests/InventoryTests.java
+++ src/test/java/dungeonmania/entities/collectableEntityTests/InventoryTests.java
@@ -1,5 +1,115 @@
 package dungeonmania.entities.collectableEntityTests;
 
-public class InventoryTests {
-    
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.dungeon.EntitiesControl;
+import dungeonmania.entities.collectableEntities.ArrowsEntity;
+import dungeonmania.entities.collectableEntities.SwordEntity;
+import dungeonmania.entities.collectableEntities.WoodEntity;
+import dungeonmania.entities.collectableEntities.BombEntity;
+import dungeonmania.entities.collectableEntities.ArmourEntity;
+import dungeonmania.util.Direction;
+import dungeonmania.entities.movingEntities.CharacterEntity;
+
+public class InventoryTests{
+    @Test
+    public void TestWoodCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        WoodEntity wood = new WoodEntity(0,0,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(wood);
+        assertTrue(entities.getEntities().size() == 1);
+
+        wood.interactWithPlayer(entities, Direction.RIGHT, player);
+
+        assertTrue(player.getInventoryInfo().size() == 1);
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
+    @Test
+    public void TestArrowsCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        ArrowsEntity arrows = new ArrowsEntity(0,0,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(arrows);
+        assertTrue(entities.getEntities().size() == 1);
+
+        arrows.interactWithPlayer(entities, Direction.RIGHT, player);
+
+        assertTrue(player.getInventoryInfo().size() == 1);
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
+    @Test
+    public void TestBombCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        BombEntity bomb = new BombEntity(0,0,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(bomb);
+        assertTrue(entities.getEntities().size() == 1);
+
+        bomb.interactWithPlayer(entities, Direction.RIGHT, player);
+
+        assertTrue(player.getInventoryInfo().size() == 1);
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
+    @Test
+    public void TestSwordCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        SwordEntity sword = new SwordEntity(0,0,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(sword);
+        assertTrue(entities.getEntities().size() == 1);
+
+        sword.interactWithPlayer(entities, Direction.RIGHT, player);
+
+        assertTrue(player.getInventoryInfo().size() == 1);
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
+    @Test
+    public void TestArmourCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        ArmourEntity armour = new ArmourEntity(0,0,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(armour);
+        assertTrue(entities.getEntities().size() == 1);
+
+        armour.interactWithPlayer(entities, Direction.RIGHT, player);
+
+        assertTrue(player.getInventoryInfo().size() > 0);
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
+    @Test
+    public void TestMultipleItemsCollect() {
+        CharacterEntity player = new CharacterEntity(0, 0, 0);
+        ArmourEntity armour = new ArmourEntity(0,0,0);
+        SwordEntity sword = new SwordEntity(0,1,0);
+        EntitiesControl entities = new EntitiesControl();
+        
+        entities.addEntities(armour);
+        entities.addEntities(sword);
+        assertTrue(entities.getEntities().size() == 2);
+
+        // Pick up first item
+        armour.interactWithPlayer(entities, Direction.RIGHT, player);
+        assertTrue(player.getInventoryInfo().size() == 1);
+        assertTrue(entities.getEntities().size() == 1);
+
+        // Pick up second item
+        sword.interactWithPlayer(entities, Direction.RIGHT, player);
+        assertTrue(player.getInventoryInfo().size() == 2);
+
+        assertTrue(entities.getEntities().size() == 0);
+    }
+
 }

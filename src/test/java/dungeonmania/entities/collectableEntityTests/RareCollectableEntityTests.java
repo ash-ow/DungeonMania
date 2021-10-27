af46d9bb9a644074c8b66086b932c4d64f3aceb2
RadarChallenger <radara350@gmail.com>
2021-10-28 00:02:43 +1100

    test: added stub test for rareCollectableEntity

--- src/test/java/dungeonmania/entities/collectableEntityTests/RareCollectableEntityTests.java
+++ src/test/java/dungeonmania/entities/collectableEntityTests/RareCollectableEntityTests.java
@@ -1,5 +1,12 @@
 package dungeonmania.entities.collectableEntityTests;
 
+import org.junit.jupiter.api.Test;
+
 public class RareCollectableEntityTests extends CollectableEntityTests {
+    // TODO add test for this class
+    @Test
+    public void test() {
+
+    }
     
 }

7c22aeb1a2c52ce1afe3c2139179224b501eafb8
RadarChallenger <radara350@gmail.com>
2021-10-31 19:19:14 +1100

    Test: IllegalArgumentException

--- src/test/java/dungeonmania/DungeonManiaControllerTests.java
+++ src/test/java/dungeonmania/DungeonManiaControllerTests.java
@@ -1,6 +1,9 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
 
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
@@ -18,5 +21,14 @@ public class DungeonManiaControllerTests {
         DungeonManiaController dungeonManiaController = new DungeonManiaController();
         DungeonResponse d1 = dungeonManiaController.newGame("advanced", "Standard");
         dungeonManiaController.tick(null, Direction.DOWN);
+    }
+
+    @Test
+    public void testIllegalArgumentException() {
+        DungeonManiaController dungeonManiaController = new DungeonManiaController();
+        assertThrows(IllegalArgumentException.class, 
+        () -> dungeonManiaController.newGame("test", "easy"));
+        assertThrows(IllegalArgumentException.class, 
+        () -> dungeonManiaController.newGame("fly", "Hard"));
     }
 }

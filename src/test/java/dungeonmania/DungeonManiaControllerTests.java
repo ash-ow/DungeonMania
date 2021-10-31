3d16cfa304a796fa9dad2d985784fab0d37bea28
RadarChallenger <radara350@gmail.com>
2021-10-31 20:35:02 +1100

    Test: cover all exeptions

--- src/test/java/dungeonmania/DungeonManiaControllerTests.java
+++ src/test/java/dungeonmania/DungeonManiaControllerTests.java
@@ -1,10 +1,12 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.ResourceLock;
 
+import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
 
@@ -24,7 +26,7 @@ public class DungeonManiaControllerTests {
     }
 
     @Test
-    public void testIllegalArgumentException() {
+    public void testIllegalArgumentExceptionFail() {
         DungeonManiaController dungeonManiaController = new DungeonManiaController();
         assertThrows(IllegalArgumentException.class, 
         () -> dungeonManiaController.newGame("test", "easy"), "gameMode is not a valid game mode");
@@ -39,5 +41,31 @@ public class DungeonManiaControllerTests {
         () -> dungeonManiaController.build("Death Star"), "buildable is not one of bow, shield");
         assertThrows(IllegalArgumentException.class, 
         () -> dungeonManiaController.interact("Execute Order 66"), "entityId is not a valid entity ID");
+    }
+
+    @Test
+    public void testIllegalArgumentExceptionPass() {
+        DungeonManiaController dungeonManiaController = new DungeonManiaController();
+        assertDoesNotThrow(() -> dungeonManiaController.newGame("test", "Hard"));
+    }
+
+    @Test
+    public void testInvalidActionExceptionFail() {
+        DungeonManiaController dungeonManiaController = new DungeonManiaController();
+        dungeonManiaController.newGame("testExeptions", "Standard");
+        dungeonManiaController.tick(null, Direction.DOWN);
+        assertThrows(InvalidActionException.class,
+        () -> dungeonManiaController.tick("health_potion", Direction.DOWN));
+        assertThrows(InvalidActionException.class,
+        () -> dungeonManiaController.build("bow"));
+    }
+
+    @Test
+    public void testInvalidActionExceptionPass() {
+        DungeonManiaController dungeonManiaController = new DungeonManiaController();
+        dungeonManiaController.newGame("testExeptions", "Standard");
+        dungeonManiaController.tick(null, Direction.DOWN);
+        assertDoesNotThrow(() -> dungeonManiaController.tick("bomb", Direction.DOWN));
+        assertDoesNotThrow(() -> dungeonManiaController.build("bow"));
     }
 }

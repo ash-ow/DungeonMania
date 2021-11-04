5e03f86edf2f6d617a315a3c0fa66aa3e594084d
Baktiar <b.niloy@student.unsw.edu.au>
2021-11-04 12:48:13 +1100

    Functionality: create SunStone entity

---
+++ src/main/java/dungeonmania/entities/collectableEntities/SunStoneEntity.java
@@ -0,0 +1,24 @@
+package dungeonmania.entities.collectableEntities;
+
+import dungeonmania.entities.Entity;
+import dungeonmania.entities.EntityTypes;
+
+public class SunStoneEntity extends CollectableEntity {
+    /**
+     * Sun Stone constructor
+     */
+    public SunStoneEntity() {
+        this(0, 0, 0);
+    }
+    
+    /**
+     * Sun Stone constructor
+     * @param x x-coordinate on the map
+     * @param y y-coordinate on the map
+     * @param layer layer on the map 
+     */
+    public SunStoneEntity(int x, int y, int layer) {
+        super(x, y, layer, EntityTypes.SUNSTONE);
+    }
+}
+

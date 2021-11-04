eb315f1281b9306a78e5b23a2c3435da22004ecb
Baktiar <b.niloy@student.unsw.edu.au>
2021-11-04 12:44:05 +1100

    Tests: Test to see if Sunstone spawns on map

---
+++ src/test/java/dungeonmania/entities/collectableEntityTests/SunStoneEntityTest.java
@@ -0,0 +1,44 @@
+package dungeonmania.entities.collectableEntityTests;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.util.Position;
+import dungeonmania.entities.EntityTypes;
+import dungeonmania.entities.collectableEntities.KeyEntity;
+
+public class SunStoneEntityTest implements ICollectableEntityTest {
+    @Override
+    @Test
+    public void TestEntityResponseInfo() {
+        SunStoneEntity sun_stone = new SunStoneEntity(0,0,0, 0);
+        assertEntityResponseInfoEquals(
+            sun_stone,
+            "sun_stone-0-0-0",
+            EntityTypes.SUN_STONE,
+            new Position(0,0),
+            false
+        );
+    }
+
+    @Override
+    @Test
+    public void TestCollect() {
+        SunStoneEntity sun_stone = new SunStoneEntity(0,0,0, 0);
+        assertEntityIsCollected(key);
+    }
+
+    @Override
+    @Test
+    public void TestUseCollectable() {
+        // test to check its function
+
+    }
+
+    @Test
+    public void TestRetain() {
+        // test to check if it remains 
+    }
+
+
+
+}

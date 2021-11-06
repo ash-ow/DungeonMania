a47a87985fe7da96accafc159c8d8b2a49e5f70e
aserravalle <ariel.serravalle@gmail.com>
2021-11-06 13:44:36 +1100

    Tests: Anduril

--- src/main/java/dungeonmania/entities/collectableEntities/AndurilEntity.java
+++ src/main/java/dungeonmania/entities/collectableEntities/AndurilEntity.java
@@ -1,5 +1,30 @@
 package dungeonmania.entities.collectableEntities;
 
-public class AndurilEntity extends SwordEntity {
+import dungeonmania.entities.EntityTypes;
+import dungeonmania.entities.movingEntities.IBoss;
+
+public class AndurilEntity extends CollectableEntity implements IWeaponEntity {
+
+    public AndurilEntity() {
+        this(0,0,0);
+    }
+
+    public AndurilEntity(int x, int y, int layer) {
+        super(x, y, layer, EntityTypes.ANDURIL);
+        this.durability = Integer.MAX_VALUE;
+    }
+
+    @Override
+    public float getDamage() {
+        return 10;
+    }
+
     
+    @Override
+    public float attack(IBattlingEntity enemy, CharacterEntity player) {
+        if (IBoss.class.isInstance(enemy)) {
+            
+        }
+        return enemy.loseHealth(player.getHealth(), this.getDamage());
+    }
 }

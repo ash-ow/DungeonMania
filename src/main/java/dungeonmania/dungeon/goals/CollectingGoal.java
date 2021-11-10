97c93ff0191d7433a265cd31c7f5243a1d97a679
Baktiar <b.niloy@student.unsw.edu.au>
2021-11-10 23:00:02 +1100

    Test: Test if sun_stone replicates treasure goal functionality & Functionality: implement sun_stone into treasure goal

--- src/main/java/dungeonmania/dungeon/goals/CollectingGoal.java
+++ src/main/java/dungeonmania/dungeon/goals/CollectingGoal.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import dungeonmania.dungeon.Dungeon;
 import dungeonmania.entities.collectableEntities.TreasureEntity;
+import dungeonmania.entities.collectableEntities.SunStoneEntity;
 
 public class CollectingGoal  implements IGoal {
     private String type;
@@ -21,7 +22,8 @@ public class CollectingGoal  implements IGoal {
      */
     public boolean checkGoal(Dungeon dungeon) {
         List<TreasureEntity> collectablesOnFloor = dungeon.getAllEntitiesOfType(TreasureEntity.class);
-        if (collectablesOnFloor.isEmpty()) {
+        List<SunStoneEntity> collectableStoneOnFloor = dungeon.getAllEntitiesOfType(SunStoneEntity.class);
+        if (collectablesOnFloor.isEmpty() && collectableStoneOnFloor.isEmpty()) {
             return true;
         }
         return false;

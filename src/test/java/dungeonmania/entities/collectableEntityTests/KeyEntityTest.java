a8cdc27d01d0a420bb8ca814b851c7b533e7bdd8
Baktiar <b.niloy@student.unsw.edu.au>
2021-11-14 19:49:08 +1100

    Tests: more tests to check only one key being picked

--- src/test/java/dungeonmania/entities/collectableEntityTests/KeyEntityTest.java
+++ src/test/java/dungeonmania/entities/collectableEntityTests/KeyEntityTest.java
@@ -2,18 +2,23 @@ package dungeonmania.entities.collectableEntityTests;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import dungeonmania.util.Position;
 import dungeonmania.entities.EntityTypes;
 import dungeonmania.entities.collectableEntities.KeyEntity;
 import dungeonmania.entities.movingEntities.CharacterEntity;
 import dungeonmania.entities.IEntity;
+import dungeonmania.entities.collectableEntities.ICollectable;
 import dungeonmania.util.Direction;
 import dungeonmania.dungeon.Dungeon;
+import dungeonmania.response.models.ItemResponse;
 
 
 public class KeyEntityTest implements ICollectableEntityTest {
@@ -38,8 +43,15 @@ public class KeyEntityTest implements ICollectableEntityTest {
     }
 
     @Override
+    @Test
     public void TestUseCollectable() {
-        // TODO Auto-generated method stub
+        ArrayList<IEntity> entities = new ArrayList<>();
+        CharacterEntity player = new CharacterEntity(0, 0);
+        KeyEntity key = new KeyEntity(0, 0, 0); 
+        entities.add(key);
+        key.used(player);
+        assertFalse(key.keyPickedUp(), "No Key has been picked up");
+        assertNull(player.getInventoryItem(key.getId()), "Inventory should not contain entity " + key.getId());
     }
 
     @Test 
@@ -54,9 +66,24 @@ public class KeyEntityTest implements ICollectableEntityTest {
         assertEquals(new Position(0, 1, 0), player.getPosition());
         assertEquals(new Position(0, 1, 0), key.getPosition());
         assertItemInInventory("key-0-1-0", player, dungeon.entitiesControl);
-        assertTrue(key.keyPickedUp(), "Key can be picked up");
+        assertNotNull(player.getInventoryItem(key.getId()), "Inventory should contain entity " + key.getId());
     }
-/*
+
+    //TO Do: Get this test to pass
+    @Test 
+    public void TestKeyPickedUp() {
+        KeyEntity key = new KeyEntity(0, 0, 0);
+        CharacterEntity player = new CharacterEntity(0, 0);
+        player.addEntityToInventory(key);
+        List<ItemResponse> inventory = player.getInventoryInfo();
+        for (ItemResponse item : inventory) {
+            assertEquals(EntityTypes.KEY.toString(), item.getType());
+        }
+        assertEquals(1, player.getInventory().size());
+        assertTrue(key.keyPickedUp(), "A key has been picked up");
+    }
+
+    //TODO: get this test to pass
     @Test 
     //This test references M2 Test13-5 "Test player cannot pickup two keys"
     public void TestCollectOnlyOneKey(){
@@ -64,19 +91,43 @@ public class KeyEntityTest implements ICollectableEntityTest {
         ArrayList<IEntity> entities = new ArrayList<>();
         KeyEntity key1 = new KeyEntity(0, 1, 1);
         KeyEntity key2 = new KeyEntity(0, 2, 2);
+        Dungeon d1 = new Dungeon(entities, "Standard", player);
         entities.add(key1);
         entities.add(key2);
         // Check that the Player has no keys initially.
-        assertTrue(!player.hasKey());
+        assertFalse(key1.keyPickedUp(), "No key should be in inventory");
         // Make Player pick up the first Key. Check that the Player now has a key.
-        player.move(Direction.DOWN);
-        assertTrue(player.hasKey());
-        assertEquals(player.getInventory().size(), 1);
-        assertNotNull(player.getInventoryItem("0"), "Key should be in inventory");
+        d1.tick(Direction.DOWN);
+        assertTrue(key1.keyPickedUp(), "Key 1 should be in inventory");
+        assertNotNull(player.getInventoryItem(key1.getId()), "Inventory should contain entity " + key1.getId());
         //Player should not be able to move down and pick up a second key i.e player cannot move into second key
-        player.move(Direction.DOWN);
+        d1.tick(Direction.DOWN);
         assertEquals(new Position(0, 1, 0), player.getPosition());
         assertEquals(new Position(0, 2, 0), key2.getPosition());
     }
-    */
+
+    @Test 
+    public void TestUnblock(){
+        CharacterEntity player = new CharacterEntity(0, 0);
+        ArrayList<IEntity> entities = new ArrayList<>();
+        KeyEntity key1 = new KeyEntity(0, 1, 1);
+        KeyEntity key2 = new KeyEntity(0, 2, 2);
+        Dungeon d1 = new Dungeon(entities, "Standard", player);
+        entities.add(key1);
+        entities.add(key2);
+        // Make Player pick up the first Key. Check that the Player now has a key.
+        d1.tick(Direction.DOWN);
+        assertTrue(key1.keyPickedUp(), "Key 1 should be in inventory");
+        assertNotNull(player.getInventoryItem(key1.getId()), "Inventory should contain entity " + key1.getId());
+        //Player uses first key 
+        key1.used(player);
+        assertFalse(key1.keyPickedUp(), "No Key has been picked up");
+        assertNull(player.getInventoryItem(key1.getId()), "Inventory should not contain entity " + key1.getId());
+        //Player can move to second key 
+        d1.tick(Direction.DOWN);
+        assertEquals(new Position(0, 2, 0), player.getPosition());
+        assertEquals(new Position(0, 2, 0), key2.getPosition());
+        assertTrue(key2.keyPickedUp(), "Key 2 should be in inventory");
+        assertNotNull(player.getInventoryItem(key2.getId()), "Inventory should contain entity " + key2.getId());
+    }   
 }

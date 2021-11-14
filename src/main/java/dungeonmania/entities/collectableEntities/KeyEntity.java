30ac9264125aaaf1e3e7381ac9fcbc90c07a78aa
Baktiar <b.niloy@student.unsw.edu.au>
2021-11-14 17:59:30 +1100

    Functionality: update functionality of key to include blocking

--- src/main/java/dungeonmania/entities/collectableEntities/KeyEntity.java
+++ src/main/java/dungeonmania/entities/collectableEntities/KeyEntity.java
@@ -4,10 +4,20 @@ import com.google.gson.JsonObject;
 
 import dungeonmania.entities.Entity;
 import dungeonmania.entities.EntityTypes;
+import dungeonmania.dungeon.EntitiesControl;
+import dungeonmania.entities.EntityTypes;
+import dungeonmania.entities.IBlocker;
+import dungeonmania.entities.IEntity;
+import dungeonmania.entities.movingEntities.CharacterEntity;
+import dungeonmania.entities.movingEntities.IMovingEntity;
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.util.Direction;
 
 
-public class KeyEntity extends CollectableEntity {
+public class KeyEntity extends CollectableEntity implements IBlocker {
     private int keyNumber;
+    protected int durability = 1;
+    private boolean keyPickedUp;
 
     /**
      * Key constructor
@@ -42,4 +52,29 @@ public class KeyEntity extends CollectableEntity {
         entityInfo.addProperty("key", this.keyNumber);
         return entityInfo;
     }
+
+    /**
+     * Picks up the key if player has no other key
+     * @param player    the characterEntity who is picking up the key
+     * @param entities  list of entities which the key will be removed from dunegeon 
+     */
+    @Override    
+    public void contactWithPlayer(EntitiesControl entities, CharacterEntity player) {
+        if (!this.keyPickedUp) {
+            player.addEntityToInventory(this);
+            entities.removeEntity(this);
+        }
+    }
+
+    @Override
+    public boolean isBlocking() {
+        return this.keyPickedUp;
+    }
+
+    @Override
+    public boolean unblockCore(IMovingEntity ent, Direction direction, EntitiesControl entitiesControl) {
+        return !this.keyPickedUp;
+      
+    }
+    
 }
